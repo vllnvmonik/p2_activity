270e1a24176932f129847a7d893f8e8a1c390aee
Monica Villanueva <vllnv.monica@gmail.com>
2024-01-14 20:53:08 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/registration', function () {
+    return view('registration');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
