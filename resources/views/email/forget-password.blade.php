70699044dcc75504490b7deb2f259d7acef7b667
Monica Villanueva <vllnv.monica@gmail.com>
2024-01-16 17:33:54 +0800

    reset pass view update

--- resources/views/email/forget-password.blade.php
+++ resources/views/email/forget-password.blade.php
@@ -1,1 +1,7 @@
-<a href="{{route('reset.password', $token)}}">Reset Password</a>
+<p>Hello,</p>
+<p>We have sent you this email in response to your request to reset your password.</p>
+<p>To reset your password, please follow the link below:</p>
+
+<button type="button" style="background-color: red; border-radius: 10px; border: none; padding: 10px; ">
+    <a href="{{route('reset.password', $token)}}" style="text-decoration: none; color: white; display: block;">Reset Password</a>
+</button>
