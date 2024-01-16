b177b5f9802a42c4518a8be052ac3a7675b2b723
Monica Villanueva <vllnv.monica@gmail.com>
2024-01-16 17:19:59 +0800

    update controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,12 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+
+
 
 class AuthController extends Controller
 {
@@ -35,7 +41,7 @@ class AuthController extends Controller
         if(Auth::attempt($userCredential)){
             return redirect()-> intended(route('welcome'));
         }
-        return redirect(route('login'))-> with("invalid", "Invalid Credential");
+        return redirect(route('login'))-> with("alert", "Invalid Credential");
     }
 
     function registrationPost(Request $request){
@@ -52,7 +58,7 @@ class AuthController extends Controller
         $user = User::create($data);
 
         if(!$user){
-            return redirect(route('registration'))-> with("invalid", "Invalid Credential");
+            return redirect(route('registration'))-> with("alert", "Invalid Credential");
         }
         return redirect(route('welcome'));
     }
@@ -60,5 +66,64 @@ class AuthController extends Controller
     function logout(){
         Auth::logout();
         return redirect(route('login'));
+    }
+
+
+    function forgetPassword(){
+        return view("forget-password");
+    }
+
+    
+    function forgetPasswordPost(Request $request){
+        $request->validate([
+            'email' => "required|email|exists:users",
+        ]);
+        $token = Str::random(64);
+
+        DB::table('password_reset_tokens')->insert([
+        'email' => $request->email,
+        'token' => $token,
+        'created_at' => Carbon::now()
+        ]);
+        
+        Mail::send("email.forget-password", ['token' => $token], function($message) use ($request){
+            $message ->to($request->email);
+            $message ->subject("Reset Password");
+
+        });
+
+        return redirect()-> to(route("forget.password"))-> with("alert", "Email has been sent to reset your password.");
+
+    }
+
+    function resetPassword($token){
+        return view("new-password", compact('token'));
+    }
+
+    function resetPasswordPost(Request $request){
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required'
+        ]);
+
+        $updatePassword = DB::table('password_reset_tokens')
+        -> where([
+            'email' => $request->email,
+            'token' => $request -> token
+        ])->first();
+
+        if(!$updatePassword){
+            return redirect()->to(route("reset.password"))-> with ("alert", "Invalid.");
+        }
+
+        User::where("email", $request->email)
+            ->update(["password"=> Hash::make($request->password)]);
+
+        DB::table('password_reset_tokens')-> where([
+            'email' => $request->email])->delete();
+
+        return redirect(route('login'))-> with("alert", "Password reset successful.");
+        
     }
 }
