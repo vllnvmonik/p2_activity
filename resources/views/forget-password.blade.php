1b3ba81a34e12052645b1c35c9aaaf8d386b347f
Monica Villanueva <vllnv.monica@gmail.com>
2024-01-16 17:18:57 +0800

    forgetpass view

---
+++ resources/views/forget-password.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+@section('title', 'Forget Password')
+@section('content')
+<div class="wrapper forget">
+    <div class="form-box">
+        <h2> Forgot Password? </h2>
+        <p class="note"> Enter your email address to reset.</p>
+        <form action="{{route('forget.password.post')}}" method="POST">
+            @csrf
+            <div class="input-field">
+                <input type="text" name="email" class="email" id="email" required>
+                <label for="email"> Email </label>
+            </div>
+            <button type="submit" class="btn"> Send Verification </button>
+        </form>
+    </div>
+</div>
+
+@if($errors->any() || session()->has('alert'))
+    <div class="alert">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        
+        @foreach($errors->all() as $error)
+            {{ $error }}
+        @endforeach
+
+        @if(session()->has('alert'))
+            {{ session('alert') }}
+        @endif
+    </div>
+@endif
+
+@endsection
